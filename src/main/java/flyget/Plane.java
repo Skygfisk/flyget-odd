8fbcdc5623e30322e1856c319e06337c7f9f62a3
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2023-01-20 14:44:08 +0100

    added a method to print the plane to the terminal, also added a static constant to help convert seat leatters to list index

--- src/main/java/flyget/Plane.java
+++ src/main/java/flyget/Plane.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Plane implements Serializable {
     private String name;
     private Seat[][] seats;
+    public static final String acb = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public Plane(int size) {
         this.seats = generateSeats(size);
@@ -22,6 +23,25 @@ public class Plane implements Serializable {
 
     public void bookSeat(int row, int seat) {
         this.seats[row][seat].setBooked(true);
+    }
+
+    public void printPlane() {
+        int rowIndex = 1;
+        char[] seatIndex = Plane.acb.substring(0, this.seats.length).toCharArray();
+        String str = "   ";
+        for (char c : seatIndex) {
+            str = str + c + "  ";
+        }
+        System.out.println(str);
+
+        for (Seat[] row : this.seats) {
+            System.out.print(rowIndex++ + " ");
+            for (Seat seat : row) {
+                char c = ((seat.getBooked()) ? 'X' : ' ');
+                System.out.print("[" + c + "]");
+            }
+            System.out.println();
+        }
     }
 
     public Seat[][] getSeats() {
