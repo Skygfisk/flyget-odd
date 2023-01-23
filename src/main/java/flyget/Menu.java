e25d5afddca64c5530206626ed7edcc673bf493a
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2023-01-23 11:15:52 +0100

    created a Person class to represent a passenger, and added methods in  Menu to create a Person instance from user input

--- src/main/java/flyget/Menu.java
+++ src/main/java/flyget/Menu.java
@@ -1,5 +1,6 @@
 package flyget;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Menu {
@@ -15,6 +16,38 @@ public class Menu {
             str = strArr[0];
         }
         return str;
+    }
+
+    public static Person getPersonFromUser() {
+        String[] strArr = getFullNameFromUser();
+        LocalDate birthDate = getBirDateFromUser();
+        Person person = new Person(strArr[0], strArr[1], birthDate);
+        System.out.println(person.getFirstName() + '\n' + person.getLastName() + '\n' + person.getBirthDate());
+        return person;
+    }
+
+    private static String[] getFullNameFromUser() {
+        System.out.print("Enter full name: ");
+        String[] strArr = sc.nextLine().trim().split("\\s");
+        return strArr;
+    }
+
+    public static LocalDate getBirDateFromUser() {
+        while (true) {
+            try {
+                System.out.print("Enter birthdate (yyyy/mm/dd): ");
+                String str = sc.nextLine().trim().replaceAll("[^0-9]", "\s");
+                String[] strArr = str.split("\s");
+                int year = Integer.parseInt(strArr[0]);
+                int month = Integer.parseInt(strArr[1]);
+                int day = Integer.parseInt(strArr[2]);
+                return LocalDate.of(year, month, day);
+            } catch (Exception e) {
+                System.out.println("Invalid birthdate");
+                continue;
+            }
+
+        }
     }
 
     public static void bookSeatOnPlane(Plane plane) {
---
+++ src/main/java/flyget/Person.java
@@ -0,0 +1,27 @@
+package flyget;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+
+    Person(String firstName, String lastName, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return this.birthDate;
+    }
+}
