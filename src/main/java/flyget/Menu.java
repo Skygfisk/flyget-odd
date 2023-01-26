bc45271cfac51a8a93163fc104406b34127f48f4
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2023-01-26 18:20:44 +0100

    added javadoc comments to public classes the methods

--- src/main/java/flyget/Menu.java
+++ src/main/java/flyget/Menu.java
@@ -1,19 +1,39 @@
 package flyget;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Scanner;
 
+/**
+ * Main class of the program
+ * handels user interface and user input
+ */
 public class Menu {
     public static Scanner sc = new Scanner(System.in);
     public static Plane plane;
+    public static File file;
+    public static String SAVE_DIRECTORY = "src\\main\\java\\flyget\\save\\";
     public static String MENU_ART = """
+
             1: Creat Plane
             2: Load Plane
             3: Book Seat
             4: List passenger
+            5: Save Plane to File
             """;
 
+    /**
+     * Asks the user for a seat in the form of a
+     * number (row) and letter (column)
+     * and converts it in to a int array with
+     * array indexes for the coresponding seat
+     * will prompt the user for another input if input is invalid
+     * 
+     * @return a int array with seat index for the seat array
+     */
     public static int[] getSeatFromUser() {
         while (true) {
             try {
@@ -34,9 +54,7 @@ public class Menu {
     public static Person getPersonFromUser() {
         String[] strArr = getFullNameFromUser();
         LocalDate birthDate = getBirDateFromUser();
-        Person person = new Person(strArr[0], strArr[strArr.length - 1], birthDate);
-        System.out.println(person.getFirstName() + '\n' + person.getLastName() + '\n' + person.getBirthDate());
-        return person;
+        return new Person(strArr[0], strArr[strArr.length - 1], birthDate);
     }
 
     public static String[] getFullNameFromUser() {
@@ -74,6 +92,13 @@ public class Menu {
         }
     }
 
+    /**
+     * Tries to book a seat on the plane
+     * with user input on what seat
+     * and to what person
+     * 
+     * @param plane the plane to book the seat on
+     */
     public static void bookSeatOnPlane(Plane plane) {
         while (true) {
             try {
@@ -95,25 +120,61 @@ public class Menu {
         }
     }
 
-    static void menu_choices(int i) throws IOException, ClassNotFoundException {
+    static void menuChoices(int i) throws IOException, ClassNotFoundException {
         switch (i) {
             case 1:
-                System.out.println("Generating new Plane");
-                plane = new SmallPlane();
+                System.out.println("1: Creat BigPlane");
+                System.out.println("2: Creat SmallPlane");
+                if (sc.hasNextInt()) {
+                    int choice = Integer.valueOf(sc.nextLine());
+                    if (choice == 1) {
+                        plane = new BigPlane();
+                        System.out.println("New BigPlane has been created");
+                    } else if (choice == 2) {
+                        plane = new SmallPlane();
+                        System.out.println("New SmallPlane has been created");
+                    }
+                }
                 break;
             case 2:
-                System.out.println("Load Plane from file");
-                System.out.println("TODO");
-                // App.mainHotel = (MyHotel) Writer.readFromFile(file);
+                try {
+                    System.out.print("Enter file to load: ");
+                    String input = sc.nextLine();
+                    file = Paths.get(SAVE_DIRECTORY + input).toFile();
+
+                    plane = (Plane) Writer.readFromFile(file);
+                    System.out.println("Plane has been loaded from file");
+                    break;
+                } catch (Exception e) {
+                    System.out.println("File not found");
+                }
                 break;
             case 3:
                 if (plane != null) {
+                    System.out.println(plane.getSeatMap());
                     bookSeatOnPlane(plane);
+                } else {
+                    System.out.println("No Plane is loaded");
                 }
                 break;
             case 4:
                 if (plane != null) {
                     System.out.println(plane.listPassengers());
+                } else {
+                    System.out.println("No Plane is loaded");
+                }
+                break;
+            case 5:
+                try {
+                    System.out.print("Enter name to save as: ");
+                    String input = sc.nextLine();
+                    file = Paths.get(SAVE_DIRECTORY + input).toFile();
+
+                    Writer.writeToFile(file, plane);
+                    System.out.println("Successfully saved Plane to file " + input);
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Could not save to file");
                 }
                 break;
             default:
@@ -121,14 +182,18 @@ public class Menu {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (Files.notExists(Paths.get(SAVE_DIRECTORY))) {
+            Files.createDirectory(Paths.get(SAVE_DIRECTORY));
+        }
+
         try {
             while (true) {
                 System.out.println(MENU_ART);
                 System.out.print("Enter a int 1-4 to choice or 'exit' to quit: ");
                 if (sc.hasNextInt()) {
                     int choice = Integer.valueOf(sc.nextLine());
-                    menu_choices(choice);
+                    menuChoices(choice);
                 } else {
                     if (sc.nextLine().equals("exit")) {
                         break;
--- src/main/java/flyget/Writer.java
+++ src/main/java/flyget/Writer.java
@@ -7,8 +7,18 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+/**
+ * A class to save and load class Objects from a file
+ */
 public class Writer {
 
+    /**
+     * Saves the given object as a file
+     * 
+     * @param file file path to save or override
+     * @param obj  class to be saved
+     * @throws IOException
+     */
     public static void writeToFile(File file, Object obj) throws IOException {
         try (FileOutputStream fileOutputStream = new FileOutputStream(file);
                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
@@ -16,6 +26,14 @@ public class Writer {
         }
     }
 
+    /**
+     * Loads a Object from the given file
+     * 
+     * @param file file path to load from
+     * @return a Object from the file
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public static Object readFromFile(File file) throws IOException, ClassNotFoundException {
         try (FileInputStream fileInputStream = new FileInputStream(file);
                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
